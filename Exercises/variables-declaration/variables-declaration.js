838921113268c2d949c15c336b23c68fdf64af52
Olha Pokolenko <olhp@ciklum.com>
2019-10-07 20:20:29 +0300

    variables

--- Exercises/variables-declaration/variables-declaration.js
+++ Exercises/variables-declaration/variables-declaration.js
@@ -1,21 +1,18 @@
 //1. Declare a variable which will be encapsulated from global scope
 
 let price = 50; 
+let discount = 17; 
 
-function TotalCount () {
-  let discount = 17; 
-  let total = price - discount; 
-
+if (price === 50) {
+  let total = price - discount;
   console.log(total);
 }
-TotalCount();
+
 //2. Declare a variable which will appear in global object.
 
-let userTemperature = 96.8;
+var userTemperature = 96.8;
 
-let fToCel = (userTemperature -32) * 5/9;
-
-console.log(fToCel);
+console.log(userTemperature);
 
 //3. Declare two variables which will be same-names but contain different values.
 let userAge = 21;
@@ -31,7 +28,6 @@ AgeValidator();
 
 //4. Declare a variable with hoisting.
 
-minTemperaturePrague = 3;
-var minTemperaturePrague; 
-
 console.log(minTemperaturePrague);
+
+var minTemperaturePrague; 
