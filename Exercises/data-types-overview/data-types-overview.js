f7b7417278281711be52d25bcc5ddd5c4735c980
Olha Pokolenko <olhp@ciklum.com>
2019-10-09 21:49:08 +0300

    fix

--- Exercises/data-types-overview/data-types-overview.js
+++ Exercises/data-types-overview/data-types-overview.js
@@ -1,13 +1,20 @@
 //1. Declare a variable on each type using two options: plain creation, creation with constructor.
-let numberVar = 32;
-let stringVar = 'Product Owner';
-let booleanVar = true;
-let nullVar = null;
+const numberVar = 32;
+const stringVar = 'Product Owner';
+const booleanVar = true;
+const nullVar = null;
 let undefinedVar; 
-let objectVar = { a: 'prior experience', b: 'resume'};
-let bigIntVar = BigInt(9007199254740991);
+const objectVar = { a: 'prior experience', b: 'resume'};
+const bigIntVar = BigInt(9007199254740991);
 
-console.log(stringVar,numberVar,booleanVar,nullVar,undefinedVar,objectVar,bigIntVar);
+class User {
+  constructor(first,last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+}
+let applicant = new User ('Michael', 'Jacobson'); 
+console.log(applicant,stringVar,numberVar,booleanVar,nullVar,undefinedVar,objectVar,bigIntVar);
 
 //2. Use operator typeof on each of them.
 
@@ -21,11 +28,13 @@ console.log(typeof bigIntVar);
 
 //3. Demonstrate object's references.
 
-objectValue = { age: 32, position: 'product owner', priorExperience: true };
-objectValue.age = 34;
-objectValue.priorExperience = false; 
+let objectValue = { age: 32, position: 'product owner', priorExperience: true };
+let anotherObjectValue = objectValue;
+anotherObjectValue.age = 34;
+anotherObjectValue.priorExperience = false; 
 
 console.log(objectValue);
+console.log(anotherObjectValue);
 
 //4. Demonstrate string's backticks features.
 
@@ -33,12 +42,20 @@ console.log(`${stringVar} is ${numberVar} years old and his CPR number is ${bigI
 
 //5. Declare an array containing different data types.
 
-let candidate = [ age = 32, position = 'product owner', priorExperience = true, gender = 'male', illness = null, bankAccount = BigInt(9007199254740991)]; 
-
+const candidate = [ age = 32, position = 'product owner', priorExperience = true, gender = 'male', bankAccount = BigInt(9007199254740991)]; 
 console.log(candidate);
 
 //6. Iterate through it and collect amount of each data type to object.
 
-candidate.forEach(function(element) {
-  console.log(typeof element);
+candidate.forEach(function(item) {
+  console.log(typeof item); 
+  console.log(Object.keys(item).lenght);
 });
+
+const array = [120, 45, 66, 88, true, false],
+  types = {};
+
+array.forEach(v => types[typeof v] = true);
+
+console.log(types);
+console.log(Object.keys(types).length);
