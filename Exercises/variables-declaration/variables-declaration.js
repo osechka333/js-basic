3695b778fb8b3a38f3a3e016ac02011bc41c7c71
Olha Pokolenko <olhp@ciklum.com>
2019-10-23 15:11:54 +0300

    generalfix

--- Exercises/variables-declaration/variables-declaration.js
+++ Exercises/variables-declaration/variables-declaration.js
@@ -1,40 +1,28 @@
 //1. Declare a variable which will be encapsulated from global scope
 
-let price = 50; 
-let discount = 17; 
+/*eslint-disable no-undef, no-var, vars-on-top, no-use-before-define*/
 
+const price = 50;
+const discount = 17;
 if (price === 50) {
-  let total = price - discount;
-  console.log(total);
+  const total = price - discount;
+  //console.log(total);
 }
-
 //2. Declare a variable which will appear in global object.
-userTemperature = 96.8;
-
-console.log(userTemperature);
-
+const userTemperature = 96.8;
+//console.log(userTemperature);
 //3. Declare two variables which will be same-names but contain different values.
 let age;
-let userAge = 21;
-
-function AgeValidator(){
-  let userAge = 17; 
-
-  age = (userAge < 18) ? 'Forbidden to vote' : 'Allowed to vote';
-
-  console.log(age);
+const userAge = 21;
+function AgeValidator() {
+  const userAge = 17;
+  age = userAge < 18 ? 'Forbidden to vote' : 'Allowed to vote';
+  //console.log(age);
 }
 AgeValidator();
-
-const a = 4; 
-{
-  const a = 5; 
-
-  console.log(a);
+const a = 4; {
+  const a = 5;
 }
-console.log(a);
-
 //4. Declare a variable with hoisting.
-
-console.log(minTemperaturePrague);
-var minTemperaturePrague; 
+//console.log(minTemperaturePrague);
+//var minTemperaturePrague
