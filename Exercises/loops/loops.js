f9c30d0c37d700c3cfefd1166ebff4079ccc4659
Olha Pokolenko <olhp@ciklum.com>
2020-01-16 00:08:44 +0200

    loopsTask

--- Exercises/loops/loops.js
+++ Exercises/loops/loops.js
@@ -1,4 +1,13 @@
-const arrayToFilter = Array.from(new Array(1000), () => {
+/*eslint-disable no-console*/
+//1. Create object and iterate through it logging pairs `key` : `value`.
+const person = {
+  gender: 'male',
+  age: 27
+};
+const entries = Object.entries(person);
+console.log(entries);
+
+const arrayToFilter = Array.from(new Array(100), () => {
   const randomValue = Math.random();
   const isNumericValue = randomValue >= 0.21;
 
@@ -16,3 +25,24 @@ const arrayToFilter = Array.from(new Array(1000), () => {
 
   return null;
 });
+console.log(arrayToFilter);
+
+//2. Filter array `arrayToFilter` from non numeric values and `NaN`.
+const results = arrayToFilter.filter(value => typeof value === 'number' && !isNaN(value));
+console.log(results);
+
+//3. Make while loop until sum of randomly generated values will be greater than `200` and save amount of iterations.
+function sum(array) {
+  let total =  0;
+  let i = 0;
+  while (total < 200) {
+    const value = array[i];
+    i++;
+    if (isNaN(value)) {
+      continue;
+    }
+    total += Number(value);
+    console.log(total, i);
+  }
+}
+sum(arrayToFilter);
